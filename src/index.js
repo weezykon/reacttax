bccb7efbb372fa75a0e3b6d6512fdd84d90c4e81
Akinbode Akinlolu Weezykon <weezykon@gmail.com>
2021-09-19 03:49:25 +0100

    installed redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// redux
+import {createStore} from 'redux'
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  reducers,
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
